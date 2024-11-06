1380127e7c57aa4db4ea12e5a148f195a125c658
Adam-Lazelle <131929157+Adam-Lazelle@users.noreply.github.com>
2024-11-06 00:11:19 -0800

    IDK Man

--- Chatroom/Simple_chatroom_code/chatclient.py
+++ Chatroom/Simple_chatroom_code/chatclient.py
@@ -1,44 +1,30 @@
 import socket
 from threading import Thread
 from datetime import datetime
-import json
 
 
 
 class Message():
 
     def __init__(self):
-        self._message_headers = {
-            'REPORT_REQUEST_FLAG':None,
-            'REPORT_RESPONSE_FLAG':None,
-            'JOIN_ACCEPT_FLAG':None,
-            'NEW_USER_FLAG':None,
-            'QUIT_REQUEST_FLAG':None,
-            'QUIT_ACCEPT_FLAG':None,
-            'ATTACHMENT_FLAG':None,
-            'NUMBER':None,
-            'USERNAME':None,
-            'FILENAME':None,
-            'PAYLOAD_LENGTH':None,
-            'PAYLOAD':None
-        }
-    
-    def set_field(self, header, value):
-        self._message_headers[header] = value
+        self.REPORT_REQUEST_FLAG = 0
+        self.REPORT_RESPONSE_FLAG = 0
+        self.JOIN_ACCEPT_FLAG = 0
+        self.NEW_USER_FLAG = 0
+        self.QUIT_REQUEST_FLAG = 0
+        self.QUIT_ACCEPT_FLAG = 0
+        self.ATTACHMENT_FLAG = 0
+        self.NUMBER = 0
+        self.USERNAME = 0
+        self.FILENAME = 0
+        self.PAYLOAD_LENGTH = 0
+        self.PAYLOAD = 0
 
-    def message(self, message):
-        self._message_headers['PAYLOAD'] = message
-
-    def compile_message(self):
-        # Compile the whole message string here
-        message_string = json.dumps(self._message_headers)
-        return message_string
-        
-
+user = Message()
 
 # Sets the preselected IP and port for the chat server
 # Eneter your machine's IP address for the host_name. Alternatively, you can enter "localhost"
-host_name = "192.168.1.1"
+host_name = "192.168.1.54"
 port = 18000
 
 # Creates the TCP socket
@@ -47,44 +33,64 @@ print("Connecting to", host_name, port, "...")
 new_socket.connect((host_name, port))
 print("Connected.")
 
-# Prompts the client for a username
-print("Type lowercase 'q' at anytime to quit!")
-name = input("Enter your a username: ")
+choice = "0"
+
+while choice != "3":
+    choice = input("Please select one of the following options:\n1. Get a report of the chatroom from the server.\n2. Request to join the chatroom.\n3. Quit the program.")
+
+    if choice == "1":
+        print("1")
+
+    elif choice == "2":
+        # Prompts the client for a username
+        print("Type lowercase 'q' at anytime to quit!")
+        name = input("Enter your a username: ")
 
 
-# Thread to listen for messages from the server
-def listen_for_messages():
-    while True:
-        message = new_socket.recv(1024).decode()
-        print("\n" + message)
+
+        # Thread to listen for messages from the server
+        def listen_for_messages():
+            while True:
+                message = new_socket.recv(1024).decode()
+                if message == "self.NUMBER":
+                    print("\n" + message)
+                else:
+                    print("\n" + message)
 
 
-t = Thread(target=listen_for_messages)
 
-t.daemon = True
+        t = Thread(target=listen_for_messages)
 
-t.start()
+        t.daemon = True
 
-# if user is an admin send the admin name before appending time and username
-if name == "admin":
-    print("Welcome Administrator")
-    new_socket.send(name.encode())
+        t.start()
 
-while True:
-    # Recieves input from the user for a message
-    to_send = input()
+        # if user is an admin send the admin name before appending time and username
 
-    # Allows the user to exit the chat room
-    if to_send.lower() == "q":
-        new_socket.send(to_send.encode())
-        exit()
-    # Appends the username and time to the clients message
-    to_send = name + ": " + to_send
-    date_now = datetime.now().strftime("[%H:%M] ")
-    to_send = date_now + to_send
 
-    # Sends the message to the server
-    new_socket.send(to_send.encode())
+        if name == "admin":
+            print("Welcome Administrator")
+            new_socket.send(name.encode())
 
-# close the socket
-new_socket.close()
+        while True:
+            # Recieves input from the user for a message
+            if(3 > user.NUMBER):
+                print(user.NUMBER)
+                to_send = input()
+
+                # Allows the user to exit the chat room
+                if to_send.lower() == "q":
+                    new_socket.send(to_send.encode())
+                    exit()
+                # Appends the username and time to the clients message
+                to_send = name + ": " + to_send
+                date_now = datetime.now().strftime("[%H:%M] ")
+                to_send = date_now + to_send
+
+                # Sends the message to the server
+                new_socket.send(to_send.encode())
+
+        # close the socket
+        new_socket.close()
+    
+exit()
